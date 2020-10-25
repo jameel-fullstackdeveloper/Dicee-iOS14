57367db3463fec11d52f92d2150e25ea538a9682
Jameel Ahmed <45817812+jameel-fullstackdeveloper@users.noreply.github.com>
2020-10-25 14:30:16 +0500

    Initial Commit

---
+++ Dicee-iOS14/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dicee-iOS14
+//
+//  Created by Jameel Ahmed on 25/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
